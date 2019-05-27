2dff49cc65f1ad48a78ee19fa63edda1d204eebe
SilentStars <h15822008350@163.com>
2019-05-27 10:29:04 +0200

    video recommendation process file.

--- Code/Recommandation/recommendation.py
+++ Code/Recommandation/recommendation.py
@@ -14,22 +14,55 @@ pd.set_option('display.width', 100)
 movies = pd.io.parsers.read_csv('/Users/houqinhan/TEDTalksDataMining/TEDTalksDataMining/Data/ted_main.csv')
 tfidf = TfidfVectorizer(max_df = 0.8, stop_words='english')
 movies['tags'] = movies['tags'].fillna('')
-tfidf_matrix = tfidf.fit_transform(movies['tags'])
-# print(tfidf_matrix)
-s = tfidf_matrix.shape
+movieList = movies['tags'].tolist()
+# print(type(movieList))
+# print(movieList[-1])
+tfidf_matrix = tfidf.fit_transform(movieList)
+# # print(tfidf_matrix)
+# s = tfidf_matrix.shape
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 # print(cosine_sim)
 indices = pd.Series(movies.index, index=movies['title']).drop_duplicates()
 
 
-def get_recommendation(title, consine_sim = cosine_sim):
-    idx = indices[title]
-    sim_scores = list(enumerate(cosine_sim[idx]))
+# def get_recommendation(title, consine_sim = cosine_sim):
+#     idx = indices[title]
+#     print(idx)
+#     print(enumerate(cosine_sim[idx]))
+#     sim_scores = list(enumerate(cosine_sim[idx]))
+#     print(sim_scores)
+#     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+#     sim_scores = sim_scores[1:11]
+#     movie_indices = [i[0]for i in sim_scores]
+#     return movies['title'].iloc[movie_indices]
+#
+#
+# ans = get_recommendation('Do schools kill creativity?')
+# print(ans)
+
+tag = "['computer', 'France', 'program']"
+# movieList.append(tag)
+# print(movieList[-1])
+# tfidf_matrix = tfidf.fit_transform(movieList)
+# s = tfidf_matrix.shape
+# cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+# print(cosine_sim)
+# indices = pd.Series(movies.index, index=movies['title']).drop_duplicates()
+# ans = get_recommendation('Do schools kill creativity?')
+# print(ans)
+
+
+def video_recommendation(input_tag):
+    movieList.append(tag)
+    tfidf_matrix_ = tfidf.fit_transform(movieList)
+    s = tfidf_matrix_.shape
+    cosine_sim_ = linear_kernel(tfidf_matrix, tfidf_matrix)
+    sim_scores = list(enumerate(cosine_sim_[-1]))
+    print(sim_scores)
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:11]
     movie_indices = [i[0]for i in sim_scores]
-    return movies['title'].iloc[movie_indices]
+    return movies['url'].iloc[movie_indices]
 
 
-ans = get_recommendation('Do schools kill creativity?')
-print(ans)
+print(video_recommendation(tag))
---
+++ Code/Recommandation/video_recommendation.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 100)
+
+movies = pd.io.parsers.read_csv('/Users/houqinhan/TEDTalksDataMining/TEDTalksDataMining/Data/ted_main.csv')
+tfidf = TfidfVectorizer(max_df=0.8, stop_words='english')
+movies['tags'] = movies['tags'].fillna('')
+movieList = movies['tags'].tolist()
+replaceList = []
+
+
+for str in movieList:
+    get_str = ""
+    get_str = str.replace('[', '')
+    get_str = get_str.replace(']', '')
+    get_str = get_str.replace(' ', '')
+    get_str = get_str.replace('\'', '')
+    replaceList.append(get_str)
+
+
+def video_recommendation(input_tag):
+    movieList.append(input_tag)
+    tfidf_matrix = tfidf.fit_transform(replaceList)
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    sim_scores = list(enumerate(cosine_sim[-1]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:2]
+    movie_indices = [i[0]for i in sim_scores]
+    result = movies['url'].iloc[movie_indices].tolist()
+    return result[0]
+
+
