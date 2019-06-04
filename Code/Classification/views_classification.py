e7371e3955746ef6d93336e36228b1d304613af8
SilentStars <h15822008350@163.com>
2019-06-04 14:55:05 +0200

    Step-3 slides.

--- Code/Classification/views_classification.py
+++ Code/Classification/views_classification.py
@@ -6,13 +6,14 @@ from Code.load_dataset import load_dataset
 import matplotlib.pyplot as plt
 import numpy as np
 from Code.get_hist import get_hist
+from yellowbrick.classifier import ClassPredictionError
 
 data = load_dataset()
 
 modelURL = '/Users/houqinhan/TEDTalksDataMining/TEDTalksDataMining/Data/model_cluster.pkl'
 km = joblib.load(modelURL)
 cluster = km.labels_.tolist()
-print(cluster)
+# print(cluster)
 
 data['cluster'] = cluster
 
@@ -27,39 +28,39 @@ get_hist(duration, 0, 2000, 20)
 
 
 def judge_views_level(views):
-    if views > 1e7:
+    if views > 2e6+5e5:
         return 0
-    elif 4e6 < views <= 1e7:
+    elif 1e6+5e5 < views <= 2e6+5e5:
         return 1
-    elif 2e6 < views <= 4e6:
+    elif 1e6 < views <= 1e6+5e5:
         return 2
-    elif 1e6 < views <= 2e6:
+    elif 6e5 < views <= 1e6:
         return 3
     else:
         return 4
 
 
-def judge_comment_level(comment):
-    if comment > 600:
+def judge_comment_level(comments):
+    if comments > 300:
         return 0
-    elif 400 < comment <= 600:
+    elif 175 < comments <= 300:
         return 1
-    elif 250 < comment <= 400:
+    elif 130 < comments <= 175:
         return 2
-    elif 150 < comment <= 250:
+    elif 70 < comments <= 130:
         return 3
     else:
         return 4
 
 
-def judge_duration_level(comment):
-    if comment > 1500:
+def judge_duration_level(durations):
+    if durations > 1500:
         return 0
-    elif 1200 < comment <= 1500:
+    elif 1200 < durations <= 1500:
         return 1
-    elif 750 < comment <= 1200:
+    elif 750 < durations <= 1200:
         return 2
-    elif 500 < comment <= 750:
+    elif 500 < durations <= 750:
         return 3
     else:
         return 4
@@ -69,17 +70,19 @@ data['views_level'] = list(map(lambda x: judge_views_level(x), data['views']))
 data['comment_level'] = list(map(lambda x: judge_comment_level(x), data['comments']))
 data['duration_level'] = list(map(lambda x: judge_duration_level(x), data['duration']))
 
-print(data.head(5))
+# print(data.head(5))
 
 train_data = data[:-500]
-print(train_data)
-test_data = data[-500:]
+# print(train_data)
+test_data = data[-1000:]
 
-target = train_data['comment_level']
-train = train_data[['cluster', 'views_level', 'duration_level']]
+target = train_data['views_level']
+train = train_data[['cluster', 'comment_level', 'duration_level']]
+# train = train_data[['views_level', 'duration_level']]
 
-target_test = test_data['comment_level']
-train_test = test_data[['cluster', 'views_level', 'duration_level']]
+target_test = test_data['views_level']
+train_test = test_data[['cluster', 'comment_level', 'duration_level']]
+# train_test = test_data[['views_level', 'duration_level']]
 
 clfs = {'svm': svm.SVC(gamma='auto'),
         'decision_tree':tree.DecisionTreeClassifier(),
@@ -105,4 +108,10 @@ for clf_key in clfs.keys():
     clf = clfs[clf_key]
     try_different_method(clf)
 
-
+classes = ["very popular", "popular", "normal", "little comments", "Nobody cares"]
+visualizer = ClassPredictionError(
+    RandomForestClassifier(), classes=classes
+)
+visualizer.fit(train, target)
+visualizer.score(train_test, target_test)
+g = visualizer.poof()
--- Code/Regression/views_regression.py
+++ Code/Regression/views_regression.py
@@ -9,27 +9,28 @@ from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
 import heapq
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as mp, seaborn
+from sklearn.linear_model import Ridge
 
 regr = linear_model.LinearRegression()
 
 data = load_dataset()
 comment = data['comments'].tolist()
 
-# n = 20
-# max_index_list = map(comment.index, heapq.nlargest(n, comment))
-# listMAX = list(max_index_list)
-# print(listMAX)
-# listMAX.sort(reverse=True)
-# print(listMAX)
-# min_index_list = map(comment.index, heapq.nsmallest(n, comment))
-# listMIN = list(min_index_list)
-#
-# for item in range(n):
-#     print(comment[listMAX[item]])
-#     comment.remove(comment[listMAX[item]])
-#     # comment.remove(comment[listMIN[item]])
-#     data = data[~data.comments.isin([comment[listMAX[item]]])]
-#     # data = data[-data.comments.isin([comment[listMIN[item]]])]
+n = 3
+max_index_list = map(comment.index, heapq.nlargest(n, comment))
+listMAX = list(max_index_list)
+listMAX.sort(reverse=True)
+min_index_list = map(comment.index, heapq.nsmallest(n, comment))
+listMIN = list(min_index_list)
+listMIN.sort(reverse=True)
+
+for item in range(n):
+    print(comment[listMAX[item]])
+    comment.remove(comment[listMAX[item]])
+    # comment.remove(comment[listMIN[item]])
+    # data = data[~data.comments.isin([comment[listMAX[item]]])]
+    # data = data[~data.comments.isin([comment[listMIN[item]]])]
 
 
 average = np.mean(comment)
@@ -39,18 +40,20 @@ print('Maximum: ' + str(max))
 min = np.min(comment)
 print('Minimum: ' + str(min) + '\n')
 
-plt.hist(comment, range=(1000,8000), bins=50, rwidth=1)
+plt.hist(comment, range=(0,1000), bins=100, rwidth=1)
 plt.show()
 
+seaborn.heatmap(data.corr(), center=0, annot=True)
+mp.show()
 train_data = data[:-500]
 test_data = data[-500:]
 
 target = train_data['comments']
-train = train_data[['duration', 'published_date', 'views']]
+train = train_data[['duration', 'languages', 'views', 'published_date']]
 # train = train_data[['views']]
 
 target_test = test_data['comments']
-train_test = test_data[['duration', 'published_date', 'views']]
+train_test = test_data[['duration', 'languages', 'views', 'published_date']]
 # train_test = test_data[['views']]
 
 regr.fit(train, target)
@@ -72,9 +75,16 @@ print('MSE(Calculate MSE according to the formula): ' + str(np.mean((regr.predic
 print('RMSE:', str(np.sqrt(mean_squared_error(target_test, regr.predict(train_test)))))
 
 
-rfr = RandomForestRegressor(max_features=3, random_state=0, n_estimators=100)
+rfr = RandomForestRegressor(max_features=4, random_state=0, n_estimators=100)
 rfr.fit(train, target)
 print("\n")
 print('MSE: ' + str(mean_squared_error(target_test, rfr.predict(train_test))))
 print('MSE(Calculate MSE according to the formula): ' + str(np.mean((rfr.predict(train_test) - target_test)**2)))
 print('RMSE:', str(np.sqrt(mean_squared_error(target_test, rfr.predict(train_test)))))
+
+ridge_clf = Ridge(alpha=0.2)
+ridge_clf.fit(train, target)
+print("\n")
+print('MSE: ' + str(mean_squared_error(target_test, ridge_clf.predict(train_test))))
+print('MSE(Calculate MSE according to the formula): ' + str(np.mean((ridge_clf.predict(train_test) - target_test)**2)))
+print('RMSE:', str(np.sqrt(mean_squared_error(target_test, ridge_clf.predict(train_test)))))
