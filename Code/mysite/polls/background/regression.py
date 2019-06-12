461f2054f481fb4b9bfdcf0b168e8974d942d61a
wzh <78910421@qq.com>
2019-06-12 23:23:53 +0200

    already finished the application

---
+++ Code/mysite/polls/background/regression.py
@@ -0,0 +1,24 @@
+from sklearn.externals import joblib
+import numpy as np
+import time
+
+modelURL = 'C:/Users/w/a1/github/TEDTalksDataMining/Data/model_regression.pkl'
+
+
+def regression_predict(input_min, input_sec, language, views, input_year, input_month, input_day):
+    duration = int(input_min) * 60 + int(input_sec)
+    r = (int(input_year), int(input_month), int(input_day), 1, 0, 0, 0, 0, 0)
+    published_date = time.mktime(r)
+    language = int(language)
+    views = int(views)
+    rfr = joblib.load(modelURL)
+    array = np.array([duration, language, views, published_date])
+    input = array.reshape(1, -1)
+
+    result = rfr.predict(input)[0]
+    return int(result)
+
+
+print(regression_predict('4', '42', '25', '1108371', '2005', '2', '14'))
+
+
--- Code/mysite/polls/views.py
+++ Code/mysite/polls/views.py
@@ -1,6 +1,6 @@
 from .models import Movie,User
 from .background import getUrl
-from .background import video_recommendation,tagManage
+from .background import video_recommendation,tagManage,regression
 from django.shortcuts import render
 from .forms import RegisterForm
 import random
@@ -13,25 +13,16 @@ def index(request): #recommendation part
         current_user_tag = request.user.tag
         current_user_id = request.user.id
         if  request.POST.get("pre") == 'like':  #like
-            print("this movie tag test->")
-            print(thismovietag)
             tags = tagManage.add(current_user_tag,thismovietag)
-            print(tags)
-            print("进行推荐用的tag《-")
             User.objects.filter(id=current_user_id).update(tag = tags) #把当前用户喜欢的tag更新
             csv_url,nextmovietag = video_recommendation.video_recommendation(tags) #获得当前视频的主页和标签，从csv来
             thismovietag[:] = nextmovietag
-            print(thismovietag)
-            print("这是我修改过后的<-")
             first_movie = getUrl.getUrl(csv_url)#获得下一个视频的url
             context = {'first_movie': first_movie}
             return render(request, 'polls/index.html', context)
         else:  #dislike
-            print("this movie tag test->")
-            print(thismovietag)
+
             tags = tagManage.delete(current_user_tag, thismovietag)
-            print(tags)
-            print("进行推荐用的tag《-")
             User.objects.filter(id=current_user_id).update(tag=tags)  # 把当前用户喜欢的tag更新
             csv_url, nextmovietag = video_recommendation.video_recommendation(tags)  # 获得当前视频的主页和标签，从csv来
             thismovietag[:] = nextmovietag
@@ -46,7 +37,21 @@ def index(request): #recommendation part
         return render(request, 'polls/index.html', context)
 
 def data(request):
-    return render(request,'polls/trailor.html')
+    if request.method == 'GET':
+        return render(request, 'polls/trailor.html')
+    else:
+        min = request.POST.get('name1', None)
+        second = request.POST.get('name2', None)
+        language = request.POST.get('name3', None)
+        views = request.POST.get('name4', None)
+        year = request.POST.get('name5', None)
+        month = request.POST.get('name6', None)
+        day = request.POST.get('name7', None)
+        number = regression.regression_predict(min,second,language,views,year,month,day)
+        context = {'number': number}
+        return render(request, 'polls/trailor.html',context)
+
+
 
 def about(request):
     return render(request,'polls/about.html')
